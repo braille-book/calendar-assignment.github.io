d2723798b1c5c63dffd41d465200abe6563fdd4d
M1cha3lJ0hns0n <mj66@hawaii.edu>
2020-07-11 19:31:15 -1000

    Updated Webpage
    Added Start and End Times and Dates

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,64 +1,104 @@
 import React from 'react';
+import DatePicker from "react-datepicker";
+import TimePicker from "react-time-picker";
 import ReactDOM from 'react-dom';
-import './style.css';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header,Form, Menu } from 'semantic-ui-react';
+import './style.css';
+import "react-datepicker/dist/react-datepicker.css";
+import { Container, Header,Form,Grid } from 'semantic-ui-react';
 
 class Calendar extends React.Component {
+  state = {
+    startDate: new Date(),
+
+  };
+
+  onChangeB = time1 => this.setState({ time1 })
+  onChangeE = time2 => this.setState({ time2 })
+
+  beginDate = date1 => {
+    this.setState({
+      startDate: date1
+    });
+  };
+
+  endDate = date2 => {
+    this.setState({
+      endDate: date2
+    });
+  };
 
   render() {
     return (
-        <Container style={{backgroundColor: "goldenrod"}} textAlign="left">
-    <Header as='h1'>Please This Form Out For Your Event!</Header>
-    <form class="ui form">
+        <Container textAlign="left">
+          <Header as='h1' textAlign="center" text="bold">Please This Form Out For Your Event!</Header>
+     <Grid>
+     <Grid.Row left>
+       <Grid.Column width={12}>
+    <Form class="ui form">
         <div class="field">
-        <label>Please Enter Date For Event</label>
-    <input placeholder="Please Enter Date For Event" />
+        <label>Please Enter Start Date For Event</label>
+          <DatePicker
+              selected={this.state.startDate}
+              onChange={this.beginDate}
+          />
     </div>
-    </form>
-    <form class="ui form">
+    </Form>
+    <Form class="ui form">
         <div class="field">
-        <label>Please Enter Time For Event</label>
-    <input placeholder="Please Enter Time For Event" />
+        <label>Please Enter Start Time For Event</label>
+          <TimePicker
+              onChange={this.onChangeB}
+              value={this.state.time1}
+          />
     </div>
-    </form>
-    <form class="ui form">
+    </Form>
+         <Form class="ui form">
+           <div class="field">
+             <label>Please Enter End Date For Event</label>
+             <DatePicker
+                 selected={this.state.endDate}
+                 onChange={this.endDate}
+             />
+           </div>
+         </Form>
+    <Form class="ui form">
         <div class="field">
-        <label>Please Enter Time Frame For Event</label>
-    <input placeholder="Please Enter Date For Event" />
+        <label>Please Enter End Time For Event</label>
+          <TimePicker
+              onChange={this.onChangeE}
+              value={this.state.time2}
+          />
     </div>
-    </form>
-    <form class="ui form">
+    </Form>
+    <Form class="ui form">
         <div class="field">
         <label>Please Enter Description For Event</label>
     <input placeholder="Please Enter the Description For Event" />
     </div>
-    </form>
-    <form class="ui form">
+    </Form>
+    <Form class="ui form">
         <div class="field">
         <label>Please Enter Location For Event</label>
     <input placeholder="Please Enter the Location For Event" />
     </div>
-    </form>
-    <form class="ui form">
+    </Form>
+    <Form class="ui form">
         <div class="field">
         <label>Invite any Guest to The Event</label>
     <input placeholder="Please Enter the Guest Email" />
     </div>
-    </form>
-    <form class="ui success form">
+    </Form>
+    <Form class="ui success form">
         <div class="field">
         <label>Email</label>
         <div class="ui input"><input type="text" placeholder="joe@schmoe.com" /></div>
         </div>
-        <div class="ui success message">
-        <div class="content">
-        <div class="header">Form Completed</div>
-    <p>You&#x27;re all signed up for the newsletter</p>
-    </div>
-    </div>
     <button class="ui button">Submit</button>
-        </form>
+        </Form>
+       </Grid.Column>
+     </Grid.Row>
+     </Grid>
 
     </Container>
   );
